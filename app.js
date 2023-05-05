e0c1644a6c2941b7259895d0aaf3aa971346edf9
christophersharpe <chrismsharpe123@gmail.com>
2023-05-05 11:11:50 +0100

    API listening on port 3000, MVC folders added

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+
+app.get("/", (req, res) => {
+    res.send('Welcome to Florin County API');
+});
+
+
+module.exports = app;
