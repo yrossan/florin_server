d0db45e1020f3724c4900da123705bce30271135
christophersharpe <chrismsharpe123@gmail.com>
2023-05-05 11:41:06 +0100

    added morgan logger

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+const logger = require('morgan')
 
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(logger('dev'))
 
 
 
