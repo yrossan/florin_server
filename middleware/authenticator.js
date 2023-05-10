80579085eb1d5da715bf604ae36c3e5ad9981ae0
christophersharpe <chrismsharpe123@gmail.com>
2023-05-10 16:02:22 +0100

    changed authenticator authorization to Authorization

--- middleware/authenticator.js
+++ middleware/authenticator.js
@@ -2,7 +2,7 @@ const Token = require("../models/Token");
 
 async function authenticator(req, res, next) {
     try {
-        const userToken = req.headers["authorization"];
+        const userToken = req.headers["Authorization"];
         if (userToken == "null") {
             throw new Error("User not authenticated.");
         } else {
