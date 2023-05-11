c2cffec71583e6dacfdf189b6d6ec1ee1393427a
christophersharpe <chrismsharpe123@gmail.com>
2023-05-11 14:57:20 +0100

    changed post delete response

--- controllers/posts.js
+++ controllers/posts.js
@@ -89,7 +89,7 @@ async function destroy (req, res) {
         const id = parseInt(req.params.id);
         const post = await Post.getOneById(id);
         const result = await post.destroy();
-        res.status(204).end();
+        res.status(204).json(`Post deleted: ${result}`);
     } catch (err) {
         res.status(404).json({"error": err.message})
     }
